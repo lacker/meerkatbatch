7b7acd059081dce467cf25deb06e452cc467ed82
Kevin Lacker <lacker@gmail.com>
2022-05-31 12:49:16 -0700

    submodule compile works

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+#include "raw/raw.h"
+
 using namespace std;
+
+const string FILENAME = "/mnt_blpd18/datax/incoming/guppi_59711_53086_001288_J0408-15-0_0001.0000.raw";
 
 int main(int argc, char* argv[]) {
   cout << "hello meerkat world\n";
